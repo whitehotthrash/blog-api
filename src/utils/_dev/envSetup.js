6dd630e0199ad133ce507ca501d16a05bb1b43a3
Tim <tim.maastricht@gmail.com>
2026-03-09 19:30:00 +1000

    feat: add scripts

---
+++ src/utils/_dev/envSetup.js
@@ -0,0 +1,9 @@
+// 1. load existing env if exists
+
+// 2. if no env exists, do env setup
+
+// 3. loop through our default values defined somewhere
+
+  // 4. for each value, add it to a string that will become file content
+
+// 5. once string is done/ loop is done, write that string to a new .env file
